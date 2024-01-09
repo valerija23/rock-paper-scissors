72ba370b391816a6878b9f5ddb4576904ec5a6ca
Ilya <karpov.ilja@gmail.com>
2024-01-09 23:42:31 +0200

    Update rps5.py

--- rps5.py
+++ rps5.py
@@ -1,6 +1,7 @@
 #
-# ROCK, PAPER, SCISSORS
+# ROCK, PAPER, SCISSORS - Iterācija 2
 # 
 # 4. Pārkopēt šeit iepriekšējo failu, modificēt spēli no iepriekšēja faila, lai ir pieci raundi un beigas rādas cik sanāca vinnēt (izmantot for loop, https://www.w3schools.com/python/python_for_loops.asp)
-# 5. Izmantot ne vairāk par trīs if/else rezultāta skaitīšanai (var izmantot ciparu starpību)
+# 5. [Bonus Level] Izmantot ne vairāk par trīs if/else rezultāta skaitīšanai (var izmantot ciparu starpību)
+# 6. [UNSTOPPABLE] Pievieno papildus ieročus Rock-Paper-Scissors-Spock-Lizard (https://en.wikipedia.org/wiki/Rock_paper_scissors#Additional_weapons)
 # 
