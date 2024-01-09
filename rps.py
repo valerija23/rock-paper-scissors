a5fac6e39bde10afab9354bddd0a52c3c09097c9
Ilya <karpov.ilja@gmail.com>
2024-01-09 23:42:51 +0200

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,10 +1,9 @@
 #
-# ROCK, PAPER, SCISSORS
+# ROCK, PAPER, SCISSORS - Iterācija 1
 # 
 # 1. Spēle prasa ievadīt ciparu no 1 līdz 3 (izmanto input, https://www.w3schools.com/python/python_user_input.asp)
 # 2. Dators iedomājas ciparu no 1 līdz 3  (izmanto random, https://www.w3schools.com/python/ref_random_randint.asp)
 # 3. Atkarībā no ievadītajiem cipariem un datora, izdrukā paziņojumu par zaudējumu, uzvaru, vai neizšķirtu (izmanto if, else https://www.w3schools.com/python/python_conditions.asp)
-# 4. Win!
 #
 
 ROCK = "1"
