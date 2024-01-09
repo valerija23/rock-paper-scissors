f094f5a280909f578e4db1c3ef2888b96609eb5d
Ilya <karpov.ilja@gmail.com>
2024-01-09 23:34:26 +0200

    Create rps5.py

---
+++ rps5.py
@@ -0,0 +1,6 @@
+#
+# ROCK, PAPER, SCISSORS
+# 
+# 4. Pārkopēt šeit iepriekšējo failu, modificēt spēli no iepriekšēja faila, lai ir pieci raundi un beigas rādas cik sanāca vinnēt (izmantot for loop, https://www.w3schools.com/python/python_for_loops.asp)
+# 5. Izmantot ne vairāk par trīs if/else rezultāta skaitīšanai (var izmantot ciparu starpību)
+# 
